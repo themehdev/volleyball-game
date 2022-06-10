0593855780df152d04d6559f7ff428257b59b41a
Michael <107226999+themehdev@users.noreply.github.com>
2022-06-10 09:50:10 -0700

    Update GLOBAL.gd

--- GLOBAL/GLOBAL.gd
+++ GLOBAL/GLOBAL.gd
@@ -12,10 +12,10 @@ var p1_score = 0
 var skill_level
 
 
-var dash_speed = 2750
+var dash_speed = 5000
 var dash_radius_factor = 0.8
 var dash_cooldown = 2
-var dash_time = 0.15
+var dash_time = 0.04
 var dash_length = dash_speed * dash_time
 var hit_power = 1200
 var gravity = 1500
