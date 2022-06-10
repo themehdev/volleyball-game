4f987104d11625af4a954e260b11981f61cad100
themehdev <107226999+themehdev@users.noreply.github.com>
2022-06-09 18:38:05 -0700

    Update ChooseEnemy.gd
    for the choosing different enemys

--- Menu Screens/ChooseEnemy.gd
+++ Menu Screens/ChooseEnemy.gd
@@ -1,5 +1,10 @@
 extends Control
-
+enum {
+	BEGINNER,
+	GOOD,
+	EXPERT
+}
+var skill_level
 
 func _ready():
 	for button in $VBoxContainer/Opponents.get_children():
@@ -11,3 +16,14 @@ func _button_pressed(button):
 
 func _on_FadeIn_finished(anim_name):
 	get_tree().change_scene("res://Level/Level.tscn")
+	GLOBAL.skill_level = skill_level
+
+
+func _on_Beginner_pressed():
+	skill_level = BEGINNER
+
+func _on_Good_pressed():
+	skill_level = GOOD
+
+func _on_Expert_pressed():
+	skill_level = EXPERT
