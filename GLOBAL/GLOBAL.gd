0bc620cb1304ddc137af5874210a7c9503386a11
themehdev <107226999+themehdev@users.noreply.github.com>
2022-06-09 17:48:49 -0700

    Update GLOBAL.gd
    for other stuff

--- GLOBAL/GLOBAL.gd
+++ GLOBAL/GLOBAL.gd
@@ -8,11 +8,20 @@ var network_player
 var has_authority = false
 var singleplayer = true
 var level
+var p1_score = 0
+enum {
+	BEGINNER,
+	GOOD,
+	EXPERT
+}
+var skill_level
 
-var dash_speed = 5000
-var dash_radius_factor = 2
+
+var dash_speed = 2750
+var dash_radius_factor = 0.8
 var dash_cooldown = 2
-var dash_time = 0.04
+var dash_time = 0.15
+var dash_length = dash_speed * dash_time
 var hit_power = 1200
 var gravity = 1500
 var friction = 6.5
